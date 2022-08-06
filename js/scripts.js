ee84c451e26bfd7dbae41420dfae450122a938ee
Incognito-Burrito <gfranco.mg@gmail.com>
2022-08-06 00:08:52 -0700

    updated sass

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,15 @@ $(function() {
         }
     });
 });
+
+$(function() {
+    $("#reserveButton").click(function(){
+        $("#reserveForm").modal("show");
+    });
+});
+
+$(function() {
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+});
